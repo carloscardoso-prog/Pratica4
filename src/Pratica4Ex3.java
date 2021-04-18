27f4dce2e7d2fb0cc18e9cfb50839d23856c919b
carlos <carloseduardocardsilva@gmail.com>
2021-04-18 15:27:10 -0300

    Pratica 4 atividades

--- src/Pratica4Ex3.java
+++ src/Pratica4Ex3.java
@@ -5,58 +5,59 @@ public class Pratica4Ex3 {
 
     public static void main(String[] args) {
 
-
+    prefeitura();
     }
 
     public static void prefeitura(){
 
-        int[][] popmedia = new int[5][2];
+        double[][] popmedia = new double[5][2];
 
         //direita salario, esquerda filhos
         System.out.println("Digite os filhos da primeira familia e em seguida o salario");
-        popmedia [0][0] = scanner.nextInt();
+        popmedia [0][0] = scanner.nextDouble();
         scanner.nextLine();
-        popmedia [0][1] = scanner.nextInt();
+        popmedia [0][1] = scanner.nextDouble();
         scanner.nextLine();
 
         System.out.println("Digite os filhos da segunda familia e em seguida o salario");
-        popmedia [1][0] = scanner.nextInt();
+        popmedia [1][0] = scanner.nextDouble();
         scanner.nextLine();
-        popmedia [1][1] = scanner.nextInt();
+        popmedia [1][1] = scanner.nextDouble();
         scanner.nextLine();
 
         System.out.println("Digite os filhos da terceira familia e em seguida o salario");
-        popmedia [2][0] = scanner.nextInt();
+        popmedia [2][0] = scanner.nextDouble();
         scanner.nextLine();
-        popmedia [2][1] = scanner.nextInt();
+        popmedia [2][1] = scanner.nextDouble();
         scanner.nextLine();
 
         System.out.println("Digite os filhos da quarta familia e em seguida o salario");
-        popmedia [3][0] = scanner.nextInt();
+        popmedia [3][0] = scanner.nextDouble();
         scanner.nextLine();
-        popmedia [3][1] = scanner.nextInt();
+        popmedia [3][1] = scanner.nextDouble();
         scanner.nextLine();
 
         System.out.println("Digite os filhos da quinta familia e em seguida o salario");
-        popmedia [4][0] = scanner.nextInt();
+        popmedia [4][0] = scanner.nextDouble();
         scanner.nextLine();
-        popmedia [4][1] = scanner.nextInt();
+        popmedia [4][1] = scanner.nextDouble();
         scanner.nextLine();
 
-        int mediasal=0, mediafil=0;
+        double mediasal=0;
+        int mediafil=0;
 
-            for (int i = 0; i < 2; i++) {
-                mediafil =+ popmedia[i][1];
-                mediasal =+ popmedia[i][0];
+            for (int i = 0; i < 5; i++) {
+                mediafil += popmedia[i][1];
+                mediasal += popmedia[i][0];
             }
 
-            mediafil = (mediafil)/popmedia.length;
+            mediafil = (mediafil)/5;
             //nao usei length antes por que esqueci e como ja fiz os codigos trocar vai demorar, desculpa professor nao tira minha nota porfavor
 
-            mediasal = (mediasal)/popmedia.length;
+            mediasal = (mediasal)/5;
 
         System.out.println("A media de filhos e " + mediafil);
-        System.out.println("E a media de salario e " + mediasal);
+        System.out.printf("E a media de salario e %.2f", mediasal);
     }
 
 }
